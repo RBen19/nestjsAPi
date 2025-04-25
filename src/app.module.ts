08355d96eefd749427cfa8ccf4de8e2f1a4a1f3e
RBen19 <bonmerciel@gmail.com>
2025-04-25 16:25:31 +0200

    mise à jour du app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CreateUserUseCase } from './application/user/use-case/create-user.use-case';
+import { PrismaService } from './infra/database/prisma/prisma.service';
+import { UserPrismaRepo } from './infra/database/prisma/repository/user-prisma.repo';
+import { UserController } from './interface/controller/user-controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController,UserController],
+  providers: [AppService,CreateUserUseCase,PrismaService,UserPrismaRepo],
 })
 export class AppModule {}
