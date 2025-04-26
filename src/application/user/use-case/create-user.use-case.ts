5dcb7d6ba657012dc8c7bf0aeb1e88b2b63c2eec
RBen19 <bonmerciel@gmail.com>
2025-04-26 06:04:42 +0200

    ajout de la fonction de login dans le use_case

--- src/application/user/use-case/create-user.use-case.ts
+++ src/application/user/use-case/create-user.use-case.ts
@@ -1,7 +1,8 @@
 import { IUserRepository } from "src/domain/user/user.repository";
 import { IPassHash } from "../interfaces/passHash";
 import { User } from "src/domain/user/user.entity";
-import { GetAllUserError, UsernameAlreadyTakenError } from "src/domain/user/error-user";
+import { GetAllUserError, InvalidCredentialsError, UsernameAlreadyTakenError } from "src/domain/user/error-user";
+
 
 export class CreateUserUseCase{
     /**
@@ -31,9 +32,27 @@ export class CreateUserUseCase{
          return users.map(user => ({
           username: user.username
          }));
-         
+
      } catch (error) {
           throw new GetAllUserError('une erreur est survenue lors du chargement de la liste ')
      }
+   }
+   async login(username:string,password:string):Promise<string>{
+
+     const user = await this.repo.getUserByUsername(username)
+     console.log(user)
+     if(user==null)
+          throw new InvalidCredentialsError()
+     console.log('verif1 passer')
+     if(!this.repo.checkUsername(username))
+          throw new InvalidCredentialsError()
+     console.log('verif 2passer')
+
+     if(!this.Hasher.compare(user!.password, password))
+          throw new InvalidCredentialsError()
+     console.log('verif3 passer')
+
+     return 'hello-world';
+   
    }
 }
