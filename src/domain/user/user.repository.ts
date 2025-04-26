89820056607d943d8be9a2ba97bec746788b72cd
RBen19 <bonmerciel@gmail.com>
2025-04-26 05:54:16 +0200

    ajout des fonctions getUserByUsername et login

--- src/domain/user/user.repository.ts
+++ src/domain/user/user.repository.ts
@@ -4,4 +4,6 @@ export interface IUserRepository{
     save(user:User):Promise<User>;
     checkUsername(username:string):Promise<boolean>
     getAllUsers():Promise<User[]>
+    getUserByUsername(username:string):Promise<User|null>
+    login(username:string,password:string):Promise<User|null>
 }
