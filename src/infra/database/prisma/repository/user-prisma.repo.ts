1cafbbf8475561c90dc41e56b32492066dc9c328
RBen19 <bonmerciel@gmail.com>
2025-04-26 05:55:39 +0200

    implémentation de l'interaction avec la base pour getUserByUsername et login dans l'infra

--- src/infra/database/prisma/repository/user-prisma.repo.ts
+++ src/infra/database/prisma/repository/user-prisma.repo.ts
@@ -3,6 +3,7 @@ import { PrismaService } from "../prisma.service";
 import { User } from "src/domain/user/user.entity";
 import { Injectable } from "@nestjs/common";
 import { PrismaUserMapper } from "../mapper/prisma-user.mapper";
+import { verify } from "crypto";
 
 @Injectable()
 export class UserPrismaRepo implements IUserRepository {
@@ -12,6 +13,25 @@ export class UserPrismaRepo implements IUserRepository {
     constructor(private readonly prisma:PrismaService) {
         
     }
+  async getUserByUsername(username: string): Promise<User|null> {
+    const getUser = await this.prisma.user.findUnique({
+      where:{
+        username:username
+      }
+    })
+    return getUser ? PrismaUserMapper.fromPrismaToDomain(getUser):null;
+  }
+  async login(username:string,password:string): Promise<User|null> {
+
+
+    const Verifuser = await this.prisma.user.findUnique({
+      where: {
+        username: username,
+      },
+    });
+
+    return Verifuser ? PrismaUserMapper.fromPrismaToDomain(Verifuser) : null;
+  }
    async getAllUsers(): Promise<User[]> {
     const users = await this.prisma.user.findMany()
     return users.map(user=> PrismaUserMapper.fromPrismaToDomain(user))
