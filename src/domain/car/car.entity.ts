bfb0f216e4bde60b31c6c7779e525d5183393885
RBen19 <bonmerciel@gmail.com>
2025-04-28 02:56:16 +0200

    ajout de l'entité metier Car

---
+++ src/domain/car/car.entity.ts
@@ -0,0 +1,8 @@
+export class Car{
+    /**
+     *
+     */
+    constructor(public readonly carId:number|null,
+        public readonly immatriculation:string,
+        public readonly userId:number) {}
+}
