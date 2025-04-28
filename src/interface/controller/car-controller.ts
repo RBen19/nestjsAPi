535701ce4fa86a86713b438e510dc5b0aeace40e
RBen19 <bonmerciel@gmail.com>
2025-04-28 19:33:09 +0200

    execution du use_case de creation

--- src/interface/controller/car-controller.ts
+++ src/interface/controller/car-controller.ts
@@ -1,16 +1,43 @@
-import { Controller, Get, UseGuards } from "@nestjs/common";
+import { Controller, Get, UseGuards,Request, Body, Response, InternalServerErrorException, Post, ConflictException } from "@nestjs/common";
 import { JwtAuthGuard } from "src/infra/auth/jwt-guard";
+import { CarDto } from "../dtos/car-dto";
+import { SaveCarUseCase } from "src/application/car/use-case/save-car.use-case";
+import { Car } from "src/domain/car/car.entity";
+import { InvalidImmatriculation } from "src/domain/car/car-error";
+import { CarImpl } from "src/infra/database/prisma/repository/car-prisma.repo.impl";
 
 @Controller('api/v1/car')
 export class CarController {
+    /**
+     *
+     */
+    constructor(private readonly saveCarUseCase:SaveCarUseCase,private readonly carRepoImpl:CarImpl  ) { 
+        this.saveCarUseCase = new SaveCarUseCase(carRepoImpl)
+    }
     @Get()
     async helloCar(){
         return 'hello from car '
     }
-    @Get('/protected')
+    @Post('/createCar')
     @UseGuards(JwtAuthGuard)
-    async helloProtected(){
-        return ' hello miss '
+    async helloProtected(@Request() req,@Body() dto:CarDto,@Response() res){
+        try {
+            const car  =  new Car(null,dto. immatriculation,req.user.user_id) 
+            await this.saveCarUseCase.execute(car)
+        
+
+            res.status(201).json({
+                message:'nouvel enregistrement réussit ',
+                                
+            })
+       
+        } catch (error) {
+            if( error instanceof InvalidImmatriculation)
+                throw new ConflictException('immatriculation déjà prise')
+
+            throw new InternalServerErrorException
+        }
+        
     }
 
 }
